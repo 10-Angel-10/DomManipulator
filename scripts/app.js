1ce3f5677ff53558cef1954f21a3f8f8c34e2396
10-Angel-10 <aocuevas123@gmail.com>
2024-11-15 17:59:02 -0800

    finished

---
+++ scripts/app.js
@@ -0,0 +1,81 @@
+// alert("Works");
+
+let btnChange1 = document.getElementById("btnChange1");
+let btnChange2 = document.getElementById("btnChange2");
+let btnChange3 = document.getElementById("btnChange3");
+let btnChange4 = document.getElementById("btnChange4");
+let btnChange5 = document.getElementById("btnChange5");
+let btnChange6 = document.getElementById("btnChange6");
+let btnChange7 = document.getElementById("btnChange7");
+let btnChange8 = document.getElementById("btnChange8");
+let btnChange9 = document.getElementById("btnChange9");
+let btnChange10 = document.getElementById("btnChange10");
+
+let affectMe1 = document.getElementById("affectMe1");
+let affectMe2 = document.getElementById("affectMe2");
+let affectMe3 = document.getElementById("affectMe3");
+let affectMe4= document.getElementById("affectMe4");
+let affectMe5 = document.getElementById("affectMe5");
+let affectMe6 = document.getElementById("affectMe6");
+let affectMe7 = document.getElementById("affectMe7");
+let affectMe8= document.getElementById("affectMe8");
+let affectMe9 = document.getElementById("affectMe9");
+let affectMe10 = document.getElementById("affectMe10");
+
+btnChange1.addEventListener("click", function(event){
+   affectMe1.innerText = "Changed the value to this string ";
+});
+
+btnChange2.addEventListener("click", function(event){
+    affectMe2.innerText = "Hope you had a great 3 day weekend";
+    affectMe2.className = "Style2";
+});
+
+ btnChange3.addEventListener("click", function(event){
+    affectMe3.innerText = "Green Bean ";
+    affectMe3.className = "Style3";
+ });
+
+ btnChange4.addEventListener("click", function(event){
+    affectMe4.innerText = "Im boxed like a fish";
+    affectMe4.className = "Style4";
+ });
+
+ btnChange5.addEventListener("click", function(event){
+    affectMe5.innerText = "Changed the value to this string ";
+    affectMe5.className = "Style5";
+ });
+
+ btnChange6.addEventListener("click", function(event){
+    affectMe6.innerText = "Check";
+    affectMe6.className = "Style6";
+ });
+
+ btnChange7.addEventListener("click", function(event){
+    affectMe7.innerText = "I just faded in ";
+    affectMe7.className = "Style7";
+ });
+
+ btnChange8.addEventListener("click", function(event){
+    affectMe8.innerText = "Im wiggling";
+    affectMe8.className = "wiggle";
+ });
+ btnChange9.addEventListener("click", function(event){
+    affectMe9.innerText = "Came out of nowhere ";
+    affectMe9.className = "zoomer";
+ });
+
+ btnChange10.addEventListener("click", function(event){
+    affectMe10.innerText = "Ouch ";
+    affectMe10.className = "barrelRoll";
+ });
+
+
+
+
+
+
+
+
+
+
